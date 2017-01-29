32faf9cdb361d977c33b2f8405db794b99d54cce
Martijn Vreeken | Kapibara Media <info@kapibara.media>
2017-01-29 11:53:49 +0100

    Refactored to use laravel-mix from 5.4

---
+++ webpack.mix.js
@@ -0,0 +1,17 @@
+const { mix } = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/assets/js/index.js', 'js')
+   .js('resources/assets/js/app.js', 'js')
+   .sass('resources/assets/sass/app/app.scss', 'css')
+   .sass('resources/assets/sass/web/style.scss', 'css');
