041024beb4aa0dbc44aa173ed1b4fd9c7d500f06
Martijn Vreeken | Kapibara Media <info@kapibara.media>
2018-07-18 15:23:21 +0200

    Footer menu dynamic, static link to terms

--- resources/views/footer/page.blade.php
+++ resources/views/footer/page.blade.php
@@ -1,35 +1,39 @@
-    <footer class="footer">
-        <div class="container">
-            <div class="columns">
-                <div class="column">
-                    <aside class="menu">
-                        <p class="menu-label">Menu</p>
-                        <ul class="menu-list">
-                            <li><a href="/">Home</a></li>
-                            <li><a href="maatwerk">Maatwerk</a></li>
-                            <li><a href="databases">Databases</a></li>
-                            <li><a href="webdevelopment">Webdevelopment</a></li>
-                            <li><a href="werkwijze">Werkwijze</a></li>
-                            <li><a href="offerte">Offerte</a></li>
-                        </ul>
-                    </aside>
-                </div>
-                <div class="column">
-                    <p class="menu-label">
-                        Contact
+<?php
+$items = [
+    "/" => "Home",
+];
+$items += \Projacked\Models\Page::all()->pluck('label', 'slug')->toArray();
+?>
+<footer class="footer">
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <aside class="menu">
+                    <p class="menu-label">Menu</p>
+                    <ul class="menu-list">
+                        @foreach($items as $link => $title)
+                        <li><a href="{{ $link }}">{{ $title }}</a></li>
+                        @endforeach
+                        <li><a href="/storage/2018-av-kapibara-media.pdf">Algemene Voorwaarden</a></li>
+                    </ul>
+                </aside>
+            </div>
+            <div class="column">
+                <p class="menu-label">
+                    Contact
+                </p>
+                <div class="content">
+                    <p>Kapibara Media
+                        <br>
+                        <a href="mailto:info@kapibara.media">info@kapibara.media</a>
+                        <br>
+                        <a href="tel:0031625431044">06 2543 1044</a>
+                        <br> {{--Henri Dunantlaan 21, 3434CJ --}}Nieuwegein
                     </p>
-                    <div class="content">
-                        <p>Kapibara Media
-                            <br>
-                            <a href="mailto:info@kapibara.media">info@kapibara.media</a>
-                            <br>
-                            <a href="tel:0031625431044">06 2543 1044</a>
-                            <br> {{--Henri Dunantlaan 21, 3434CJ --}}Nieuwegein
-                        </p>
-                        <p>KvK te Utrecht: 67362656{{--<br>
-                        Belastingnummer: NL186456621B02--}}</p>
-                    </div>
+                    <p>KvK te Utrecht: 67362656{{--<br>
+                    Belastingnummer: NL186456621B02--}}</p>
                 </div>
             </div>
         </div>
-    </footer>
+    </div>
+</footer>
