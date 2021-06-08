dd1add1971d9d1c6c580a23343b4b7f913a806f6
Martijn Vreeken | Kapibara Media <martijn@kapibara.media>
2021-06-08 11:20:54 +0200

    Placed Mamoto code in wrong file

--- resources/views/partial/head.blade.php
+++ resources/views/partial/head.blade.php
@@ -5,6 +5,20 @@
     <meta name="description" content="Kapibara Media is een webbureau in het midden van het land. We zijn gespecialiseerd in het realiseren van maatwerk weboplossingen.">
     <title>@yield('title')</title>
     <link rel="stylesheet" type="text/css" href="/css/style.css">
-    
+    <!-- Matomo -->
+    <script type="text/javascript">
+    var _paq = window._paq = window._paq || [];
+    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+        var u="//matomo.kapibara.media/";
+        _paq.push(['setTrackerUrl', u+'matomo.php']);
+        _paq.push(['setSiteId', '1']);
+        var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+        g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    })();
+    </script>
+    <!-- End Matomo Code -->
 </head>
 
